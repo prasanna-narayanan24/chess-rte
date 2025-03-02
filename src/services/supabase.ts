431a0d7964fe3a0290624b8ac78213de503e64b9
Prasanna <prasanq24n97@gmail.com>
2025-03-02 22:09:37 +0530

    [ CHORE ] added logs

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = (window as any)?.configs?.VITE_SUPABASE_BASE_URL || import.meta.env.VITE_SUPABASE_BASE_URL;
 const SUPABASE_ANON_KEY = (window as any)?.configs?.VITE_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+
+console.log('SUPABASE_URL :>> ', SUPABASE_URL);
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export async function signInAnonymously(name?: string) {
